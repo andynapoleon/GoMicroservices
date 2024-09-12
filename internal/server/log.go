61615ef64b9680254bd8db0f7e8fb9f9e59194a2
Andy Tran <anhquoctran006@gmail.com>
2024-09-12 12:56:45 -0600

    get started with commit log

--- internal/server/log.go
+++ internal/server/log.go
@@ -5,8 +5,9 @@ import (
 	"sync"
 )
 
+// Log object is just a slice of records
 type Log struct {
-	mu      sync.Mutex
+	mu      sync.Mutex // mutex lock
 	records []Record
 }
 
@@ -18,7 +19,7 @@ func NewLog() *Log {
 // Append a record to the end of the log
 func (c *Log) Append(record Record) (uint64, error) {
 	c.mu.Lock()
-	defer c.mu.Unlock()
+	defer c.mu.Unlock() // this will be called when the function returns
 	record.Offset = uint64(len(c.records))
 	c.records = append(c.records, record)
 	return record.Offset, nil
